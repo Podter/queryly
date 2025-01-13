6187be3b7edd787052111faddb415ea2bf411bbc
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-13 23:17:50 +0700

    more astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://queryly.podter.me",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
