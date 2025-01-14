d59b7f7ffe1f016255540b20acd2238bb3b1591f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-14 23:47:17 +0700

    added container

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,13 @@ export default {
   darkMode: ["class"],
   content: ["./src/**/*.{astro,js,jsx,ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1536px",
+      },
+    },
     extend: {
       borderRadius: {
         lg: "var(--radius)",
