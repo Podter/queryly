cc77940e1fcd8e9e217490564a227b138b0b2bf7
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-15 22:11:46 +0700

    added system prompt

--- src/pages/api/llm.ts
+++ src/pages/api/llm.ts
@@ -5,6 +5,7 @@ import {
 } from "ai";
 
 import { cacheMiddleware, groq } from "~/lib/ai";
+import systemPrompt from "~/lib/prompt.txt?raw";
 import { omitResult, search } from "~/lib/searxng";
 
 export const POST: APIRoute = async ({ request, clientAddress }) => {
@@ -16,9 +17,19 @@ export const POST: APIRoute = async ({ request, clientAddress }) => {
       model: groq("llama-3.1-8b-instant"),
       middleware: cacheMiddleware(clientAddress),
     }),
-    system:
-      "You are a helpful assistant. You need to summarize the search results.",
-    prompt: JSON.stringify(searchResult, null, 2),
+    maxTokens: 4096,
+    system: systemPrompt,
+    messages: [
+      {
+        role: "user",
+        content: JSON.stringify(searchResult, null, 2),
+      },
+      {
+        role: "assistant",
+        content: "<summary>",
+      },
+    ],
+    stopSequences: ["</summary>"],
   });
 
   return result.toDataStreamResponse();
