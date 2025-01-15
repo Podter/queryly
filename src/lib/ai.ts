888d1d433eb515fd3671ebb31fc264e54afd3acc
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-15 16:32:20 +0700

    added model caching

---
+++ src/lib/ai.ts
@@ -0,0 +1,68 @@
+import { deserialize, serialize } from "node:v8";
+import type {
+  Experimental_LanguageModelV1Middleware as LanguageModelV1Middleware,
+  LanguageModelV1StreamPart,
+} from "ai";
+import { createGroq } from "@ai-sdk/groq";
+import { simulateReadableStream } from "ai";
+import { GROQ_API_KEY } from "astro:env/server";
+import { objectHash } from "ohash";
+
+import { redis } from "./cache";
+
+export const groq = createGroq({
+  apiKey: GROQ_API_KEY,
+});
+
+export const cacheMiddleware: LanguageModelV1Middleware = {
+  wrapStream: async ({ doStream, params }) => {
+    const cacheKey = `ai:${objectHash(params)}`;
+
+    // Check if the result is in the cache
+    const cached = await redis.getBuffer(cacheKey);
+
+    // If cached, return a simulated ReadableStream that yields the cached result
+    if (cached !== null) {
+      // Format the timestamps in the cached response
+      const formattedChunks = (
+        deserialize(cached) as LanguageModelV1StreamPart[]
+      ).map((p) => {
+        if (p.type === "response-metadata" && p.timestamp) {
+          return { ...p, timestamp: new Date(p.timestamp) };
+        } else return p;
+      });
+      return {
+        stream: simulateReadableStream({
+          initialDelayInMs: 5000,
+          chunks: formattedChunks,
+        }),
+        rawCall: { rawPrompt: null, rawSettings: {} },
+      };
+    }
+
+    // If not cached, proceed with streaming
+    const { stream, ...rest } = await doStream();
+
+    const fullResponse: LanguageModelV1StreamPart[] = [];
+
+    const transformStream = new TransformStream<
+      LanguageModelV1StreamPart,
+      LanguageModelV1StreamPart
+    >({
+      transform(chunk, controller) {
+        fullResponse.push(chunk);
+        controller.enqueue(chunk);
+      },
+      async flush() {
+        // Store the full response in the cache after streaming is complete
+        await redis.set(cacheKey, serialize(fullResponse));
+        await redis.expire(cacheKey, 3600); // 1 hour
+      },
+    });
+
+    return {
+      stream: stream.pipeThrough(transformStream),
+      ...rest,
+    };
+  },
+};
--- src/pages/api/llm.ts
+++ src/pages/api/llm.ts
@@ -1,20 +1,21 @@
 import type { APIRoute } from "astro";
-import { createGroq } from "@ai-sdk/groq";
-import { streamText } from "ai";
-import { GROQ_API_KEY } from "astro:env/server";
+import {
+  streamText,
+  experimental_wrapLanguageModel as wrapLanguageModel,
+} from "ai";
 
+import { cacheMiddleware, groq } from "~/lib/ai";
 import { omitResult, search } from "~/lib/searxng";
-
-const groq = createGroq({
-  apiKey: GROQ_API_KEY,
-});
 
 export const POST: APIRoute = async ({ request }) => {
   const { prompt } = (await request.json()) as { prompt: string };
   const searchResult = omitResult(await search(prompt, { pageno: 1 }));
 
   const result = streamText({
-    model: groq("llama-3.1-8b-instant"),
+    model: wrapLanguageModel({
+      model: groq("llama-3.1-8b-instant"),
+      middleware: cacheMiddleware,
+    }),
     system:
       "You are a helpful assistant. You need to summarize the search results.",
     prompt: JSON.stringify(searchResult, null, 2),
